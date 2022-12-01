34dad4df337d95606ded7d0375905d08164d9ec6
Frederik Vanhee <Frederik.Vanhee@theScriptingEngineer.com>
2022-12-01 17:24:22 +0100

    Demo used in YouTube video

--- demo.py
+++ demo.py
@@ -12,6 +12,7 @@ theSession = NXOpen.Session.GetSession()
 basePart: NXOpen.BasePart = theSession.Parts.BaseWork
 theLW: NXOpen.ListingWindow = theSession.ListingWindow
 
+
 def GetSolution(solutionName: str) -> Union[NXOpen.CAE.SimSolution, None]:
     """This function returns the SimSolution object with the given name.
     Returns None if not found, so the user can check and act accordingly
@@ -26,23 +27,25 @@ def GetSolution(solutionName: str) -> Union[NXOpen.CAE.SimSolution, None]:
     NXOpen.CAE.SimSolution or None
         The FIRST solution object with the given name if found, None otherwise
     """
-
     simPart: NXOpen.CAE.SimPart = cast(NXOpen.CAE.SimPart, basePart) # explicit casting makes it clear
-    simSimulation: NXOpen.CAE.SimSimulation = simPart.Simulation
+    simSimulation = simPart.Simulation
     simSolutions: List[NXOpen.CAE.SimSolution] = simSimulation.Solutions.ToArray()
 
     simSolution: List[NXOpen.CAE.SimSolution] = [item for item in simSolutions if item.Name.lower() == solutionName.lower()]
     if len(simSolution) == 0:
-        # solution not found
+        # solution with the given name not found
         return None
     
     # return the first simSolution with the requested name
     return simSolution[0]
 
-
 def main() :
     theLW.Open()
     theLW.WriteFullline("Starting Main() in " + theSession.ExecutingJournal)
+
+    mySolution: NXOpen.CAE.SimSolution = GetSolution("SolutionName")
+    if mySolution != None:
+        print(mySolution.JournalIdentifier)
 
 if __name__ == '__main__':
     main()
