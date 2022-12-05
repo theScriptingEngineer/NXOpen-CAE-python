4ecf5f996d7b5fe8a6ea0487f4d89065e560523a
Frederik Vanhee <Frederik.Vanhee@theScriptingEngineer.com>
2022-12-05 09:14:53 +0100

    Updated with instrucions for intellisense

--- README.md
+++ README.md
@@ -13,19 +13,21 @@ If you are interested in learning NXOpen, please check out my course on Udemy:
 >Moreover, Siemens advises agains using Python if you are starting with NXOpen.
 
 
-## Using code completion (aka intellisense) for NXOpen in python
+## Using intelligent code completion (aka intellisense) for NXOpen in python
 
 One of the advantages of python is that it is dynamically typed. The consequence is that the IDE (eg. PyCharm, VSCode) does not know
 the object type of the variable.
 While writing NXOpen code in python I found this a huge disadvantage, because during coding I rely heavily on code completion (aka intellisense)
 to know the available methods on the objects.
-Apart from an older document from siemens explaining how to get this to work with Eclipse and PyDev add-on, I'm not aware of any other resource to get code completion to work.
+Apart from an older document from siemens explaining how to get this to work with Eclipse and PyDev add-on, which only works partially, I'm not aware of any other resource to get code completion to work.
+
 This is why I created my own method (a work in progress, not perfect but still handy) which is  independent from the IDE you are using.
-You can find more information on how to set this up using this link
-[Code completion in python](https://youtu.be/ODsZF7x7UoQ)
+This is why I created my own stub files for the NXOpen libraries. 
 
 
-## Type checking in VSCode
+### Visual Studio Code
+
+Demo: [Code completion in python (VSCode)](https://youtu.be/ODsZF7x7UoQ)
 
 From [this blog](https://www.emmanuelgautier.com/blog/enable-vscode-python-type-checking)
 
@@ -33,6 +35,22 @@ In VSCode go to settings.json (ctrl + P -> settings.json)
 or locate settings.json under the folder .vscode an add the following entry:
 *"python.analysis.typeCheckingMode": "basic"*
 
+In VSCode go to settings.json (ctrl + P -> settings.json) 
+or locate settings.json under the folder .vscode an add the following entry:
+
+**"python.analysis.stubPath": "path_to_the_stub_files/Release2023/"**
+
+Please contact me for a copy of the stub files.
+
+Successful configuration should give no errors after opening **intellisense.py** (might need to restart VSCode)
+
+### PyCharm
+
+Demo: [Code completion in python (PyCharm)](https://youtu.be/468SGBALQQM)
+
+Add the location of the stub files to the interpreter path. Instructions on how to do this can be found [here](https://www.jetbrains.com/help/pycharm/installing-uninstalling-and-reloading-interpreter-paths.html)
+
+Please contact me for a copy of the stub files.
 
 ## Local python environment
 
---
+++ intellisense.py
@@ -0,0 +1,66 @@
+# intellisense by theScriptingEngineer (www.theScriptingEngineer.com)
+# NXOpen Python Reference Guide:
+# https://docs.plm.automation.siemens.com/data_services/resources/nx/1899/nx_api/custom/en_US/nxopen_python_ref/index.html
+
+# This file is used to demonstrate a working intellisense (code completion) for 
+# writing NXOpen journals using Python
+
+import math
+import sys
+import NXOpen
+import NXOpen.CAE
+from typing import List, cast, Optional, Union
+
+theSession: NXOpen.Session = NXOpen.Session.GetSession()
+basePart = theSession.Parts.BaseWork
+theLW: NXOpen.ListingWindow = theSession.ListingWindow
+
+def get_solution(solution_name: str) -> Union[NXOpen.CAE.SimSolution, None]:
+    """This function returns the SimSolution object with the given name.
+    Returns None if not found, so the user can check and act accordingly
+
+    Parameters
+    ----------
+    solution_name: int
+        The name of the solution to return, case insensitive
+
+    Returns
+    -------
+    NXOpen.CAE.SimSolution or None
+        The FIRST solution object with the given name if found, None otherwise
+    """
+    # The next commented block is an extended example where each object is typed.
+    # sim_part: NXOpen.CAE.SimPart = cast(NXOpen.CAE.SimPart, basePart)  # explicit casting makes it clear
+    # sim_simulation: NXOpen.CAE.SimSimulation = sim_part.Simulation
+    # sim_solution_collection: NXOpen.CAE.SimSolutionCollection = sim_simulation.Solutions
+    # sim_solutions: List[NXOpen.CAE.SimSolution] = sim_solution_collection.ToArray()
+    # sim_solution: List[NXOpen.CAE.SimSolution] = [item for item in sim_solutions if item.Name.lower == solution_name.lower()]
+
+    # # minimal working example PyCharm
+    # sim_part: NXOpen.CAE.SimPart = basePart  # simply typing the sim_part provides intellisense.
+    # sim_solutions: List[NXOpen.CAE.SimSolution] = sim_part.Simulation.Solutions.ToArray()
+
+    # # minimal working example Visual Studio Code
+    sim_part = cast(NXOpen.CAE.SimPart, basePart)  # explicit casting required in Visual Studio Code
+    sim_solutions = sim_part.Simulation.Solutions.ToArray()
+
+    sim_solution = [item for item in sim_solutions if item.Name.lower() == solution_name.lower()]
+    if len(sim_solution) == 0:
+        # solution with given name not found
+        return None
+
+    # return the first solution with the requested name
+    return sim_solution[0]
+
+
+def main():
+    theLW.Open()
+    theLW.WriteFullline("Starting Main() in " + theSession.ExecutingJournal)
+
+    my_solution: NXOpen.CAE.SimSolution = get_solution("my_solution_name")
+    if my_solution is not None:
+        print(my_solution.JournalIdentifier)
+
+
+if __name__ == '__main__':
+    main()
